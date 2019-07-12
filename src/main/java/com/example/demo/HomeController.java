ce0b03e1a4564818976dc24cbd4577e4b128da69
fdmckie <fdmckie@gmail.com>
2019-07-12 16:49:05 -0400

    added controller

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,7 +1,8 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class HomeController {
     @RequestMapping("/")
     public String homePage(){
